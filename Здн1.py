1b9368b20fb3042d0eb6f64d80570f7454888daf
Fisik-Yadershik <70748393+Fisik-Yadershik@users.noreply.github.com>
2022-10-25 10:39:57 +0200

    Add files via upload

--- Здн1.py
+++ Здн1.py
@@ -8,27 +8,10 @@
 соединение, строк. В остальных случаях введенные числа суммируются.
 """
 
-
-class Summ():
-    def __init__(self, inp1, inp2):
-        self.a = inp1
-        self.b = inp2
-
-    def summ(self):
-        if (self.a.isnumeric()==True) and (self.b.isnumeric==True):
-            print("Сумма чисел: ", int(self.a) + int(self.b))
-        else:
-            print("Результат конкатенации: ", self.a + self.b)
-
-    def cont(self):
-        print("Результат конкатенации: ", self.a + self.b)
-
-
-def main():
-    inp1 = input("Введите первое число: ")
-    inp2 = input("Введите второе число: ")
-    summ = Summ(inp1, inp2)
-    summ.summ()
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    try:
+        first = input("Введите первое число - ")
+        second = input("Введите второе число - ")
+        print(first + second)
+    except:
+        print(str(first + second))
